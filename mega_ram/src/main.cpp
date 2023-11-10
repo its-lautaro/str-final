0e1982b115dd141446e4d866600d2e8dc8715b66
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-11-10 00:34:57 -0300

    add program

--- mega_ram/src/main.cpp
+++ mega_ram/src/main.cpp
@@ -1,29 +1,83 @@
 /*
+*
 * El tamaño máximo de memoria disponible para matrices en ram es
-8 Kb
-
-Se utilizaran matrices de enteros de 8bits de tamaño 52x52 (2704 elementos)
+* 8 Kb
+*
 */
 #include <avr/io.h>
 #include <stdlib.h>
+#include <util/delay.h>
+#include <uart.h>
+#include <timer.h>
+#define F_CPU 16000000
 
-uint16_t const n = 52;
-uint8_t A[n*n];
-uint8_t B[n*n];
-uint8_t C[n*n];
+uint8_t const n = 4;
+uint8_t* A, * B, * C;
 
-int main() {
-  DDRB |= (1 << 7); //led
-  for (uint16_t i = 0; i < n*n; i++) {
-    A[i] = i % 9;
-    B[i] = i % 9;
-    C[i] = i % 9;
-  }
-
+// blink builtin led
+void error() {
   while (1) {
-    for (uint16_t i = 0; i < n*n; i++) {
-      C[i] += A[i] * B[i];
+    PORTB |= (1 << 7);
+    _delay_ms(20);
+    PORTB &= ~(1 << 7);
+    _delay_ms(200);
+  }
+}
+
+// perform matrix product
+void multiplicar() {
+  for (uint8_t i = 0; i < n; i++) {
+    for (uint8_t j = 0; j < n; j++) {
+      for (uint8_t k = 0; k < n; k++) {
+        C[i * n + j] += A[i * n + k] * B[j * n + k];
+      }
     }
   }
+}
+
+// Validates operation result
+void validar() {
+  for (uint16_t i = 0; i < n * n; i++) {
+    if (C[i] != n) {
+      error(); //bloqueante
+    }
+  }
+}
+
+int main() {
+  DDRB |= (1<<7); // Led
+  UART_Init();
+
+  A=(uint8_t*)malloc(n*n*sizeof(uint8_t));
+  B=(uint8_t*)malloc(n*n*sizeof(uint8_t));
+  C=(uint8_t*)malloc(n*n*sizeof(uint8_t));
+
+  // Check if there was enough memory
+  if (A == NULL || B==NULL || C == NULL){
+    error();
+  }
+
+  // Initialize matrices
+  for (int i = 0; i < n*n; i++)
+  {
+    A[i]=1;
+    B[i]=1;
+    C[i]=0;
+  }
+  timer1_init();
+  
+  multiplicar();
+  uint32_t time = timer1_getCount();
+  
+  validar();
+  
+  // Print elapsed time
+  UART_PrintStr("Tiempo transcurrido: ");
+  UART_PrintNumber(time);
+  UART_PrintStr(" us\n");
+  
+  while (1) {
+  }
+
   return 0;
 }
