a258871bdf6c6208730b514f026be155bf2e07aa
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-11-10 16:16:44 -0300

    fix timer

--- mega_ram/src/timer.cpp
+++ mega_ram/src/timer.cpp
@@ -31,7 +31,7 @@ void timer1_start(uint16_t prescaler) {
 }
 
 uint32_t timer1_stop(){
-    TCCR1B=0;
+    TCCR1B &= ~(1<<CS10) & ~(1<<CS11) & ~(1<<CS12);
     uint32_t time = TCNT1;
     time *= fac;
     return time;
