c2588f314da8ca6ad2bf66744e98f5fb5ee51ab0
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-12-12 23:53:12 -0300

    final

--- DUE/src/main.cpp
+++ DUE/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-uint8_t n = 8; // Cant filas/columnas
-uint8_t bs = 2; // Tamaño de bloque
+uint8_t n = 176; // Cant filas/columnas
+uint8_t bs = 11; // Tamaño de bloque
 uint8_t fil_disponible = 0; // Proxima fila a procesar
+uint8_t col_esclavo = 0; // Proxima columna de bloques B a enviar al esclavo
+uint8_t fil_esclavo = 255; // Ultima fila de bloques A enviada al esclavo
+bool esperando_resultados = false; // Si se espera recibir info del esclavo
 uint8_t* A, * B, * C;
 uint32_t start_time, elapsed_time;
 
-
-// Bytes reservados para control de flujo de la transmision
-#define _WAIT_ 255
-#define _RESUME_ 254
+// Byte reservados para control de flujo de la transmision
+#define _JOB_REQ_ 255
 
 void error() {
   while (1) {
@@ -22,30 +23,108 @@ void error() {
 // Validar resultado de la operacion
 void validar() {
   bool error_ = false;
-  Serial.println("Validando matriz");
+  //Serial.println("Validando matriz");
   for (int i = 0; i < n; i++) {
-    Serial.print("C: ");
+    //Serial.print("C: ");
     for (int j = 0; j < n; j++) {
       if (C[i * n + j] != n) error_ = true;
-      Serial.print(C[i * n + j]);
-      Serial.print(" ");
+      //Serial.print(C[i * n + j]);
+      //Serial.print(" ");
     }
-    Serial.print('\n');
+    // Serial.print('\n');
   }
   if (error_) {
     Serial.println("Error: fallo en la validacion");
     error(); //bloqueante
   }
-  while(1){;}
+  else {
+    Serial.print("Exito");
+
+  }
+  while (1) { delay(1000); }
 }
-/*
-* Calcula una fila de bloques de la matriz resultado mientras chequea por pedidos de trabajo
-*/
+// Enviar columna de bloques B
+void enviarCol() {
+  for (int i = col_esclavo; i < (col_esclavo + bs); i++) {
+    for (int j = 0; j < n; j++) {
+      Serial3.write(B[i * n + j]);
+    }
+  }
+  col_esclavo += bs;
+}
+// Enviar fila de bloques A
+void enviarFila() {
+  fil_esclavo = fil_disponible;
+  for (int i = fil_esclavo; i < (fil_esclavo + bs); i++) {
+    for (int j = 0; j < n; j++) {
+      Serial3.write(A[i * n + j]);
+    }
+  }
+  fil_disponible += bs;
+}
+
+void recibirBloque() {
+  // Maestro avisa que esta listo para recibir
+  //Serial.println("Recibiendo trabajo");
+  Serial3.write(_JOB_REQ_);
+  // Recibir bloque resultado
+  uint8_t blk[bs * bs];
+  if (Serial3.readBytes(blk, bs * bs) < bs * bs) {
+    Serial.println("Error recibiendo bloque resultado");
+    error();
+  }
+  // Guardar bloque resultado
+  for (int i = 0; i < bs; i++) {
+    for (int j = 0; j < bs; j++) {
+      C[(fil_esclavo + i) * n + ((col_esclavo - bs) + j)] = blk[i * bs + j];
+    }
+  }
+  // Imprimir bloque recibido
+  // for (int i = 0; i < bs; i++) {
+  //   Serial.print("C: ");
+  //   for (int j = 0; j < bs; j++) {
+  //     Serial.print(blk[i * bs + j]);
+  //     Serial.print(" ");
+  //   }
+  //   Serial.println();
+  // }
+  //Continuar procesando la peticion de trabajo
+  Serial3.write(_JOB_REQ_);
+}
+
+void enviarTrabajo() {
+  //Serial.println("Pedido de trabajo..");
+  // Si hay resultados que deben llegar del esclavo los enviará antes de pedir trabajo
+  if (esperando_resultados) {
+    esperando_resultados = false;
+    recibirBloque();
+  }
+  // Esto es importante que se haga aca porque cuando espero resultados necesito sabe cual fue la ultima columna enviada (y si fue n y la reinicio ante pierdo esa info)
+  if (col_esclavo == n) {
+    col_esclavo = 0;
+  }
+  // Si debe arrancar a procesar una fila nueva y hay filas disponibles
+  if (col_esclavo == 0 && fil_disponible < n) {
+    enviarFila();
+    enviarCol();
+    esperando_resultados = true;
+  }
+  // Si esta actualmente procesando una fila
+  else if (col_esclavo != 0) {
+    // Enviar siguiente columna
+    enviarCol();
+    esperando_resultados = true;
+  }
+}
+// Calcula una fila de bloques de la matriz resultado mientras chequea por pedidos de trabajo
 void calcular_fila() {
-  for (uint8_t i = fil_disponible; i < (fil_disponible + bs); i++) {
+  uint8_t fil_maestro = fil_disponible;
+  fil_disponible += bs;
+  for (uint8_t i = fil_maestro; i < (fil_maestro + bs); i++) {
     for (uint8_t j = 0; j < n; j++) {
       for (uint8_t k = 0; k < n; k++) {
         C[i * n + j] += A[i * n + k] * B[j * n + k];
+        if (Serial3.read() == _JOB_REQ_) enviarTrabajo();
       }
     }
   }
@@ -53,9 +132,9 @@ void calcular_fila() {
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); // Led Debug
-  Serial.begin(115200); // Inicializa el puerto para comunicacion serial
-  Serial3.begin(115200); // Comunicacion Arduino
-
+  Serial.begin(115200UL); // Inicializa el puerto para comunicacion serial (debug)
+  Serial3.begin(250000UL); // Comunicacion Arduino
+  Serial.println("Enviando informacion inicial");
   // Alocacion e inicializacion
   A = (uint8_t*)malloc(n * n * sizeof(uint8_t));
   B = (uint8_t*)malloc(n * n * sizeof(uint8_t));
@@ -66,43 +145,33 @@ void setup() {
       B[i * n + j] = 1;
     }
   }
-  Serial.println("Enviando valores iniciales");
+  //Serial.println("Enviando valores iniciales");
   Serial3.write(n);
   Serial3.write(bs);
-  Serial.println("Esperando confirmacion");
-
-  while (Serial3.read() != _RESUME_) { ; }
-  // Enviar fila de bloques A
-  for (int i = 0; i < bs; i++) {
-    for (int j = 0; j < n; j++) {
-      Serial3.write(A[i * n + j]);
-    }
-  }
-  // Enviar matriz B
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      Serial3.write(B[i * n + j]);
-    }
-  }
-  Serial.println("Matrices enviadas");
-  fil_disponible += bs;
+  //Serial.println("Esperando confirmacion");
+  while (Serial3.read() != _JOB_REQ_) { ; }
+  start_time = millis();
+  enviarTrabajo();
 }
 
 void loop() {
   if (fil_disponible < n) {
-    Serial.print("Calculando fila: ");
-    Serial.println(fil_disponible);
+    //Serial.print("Calculando fila: ");
+    //Serial.println(fil_disponible);
     calcular_fila();
-    fil_disponible += bs;
   }
   else {
-    // Finalizo el cálculo, pedir resultados a esclavo
-    Serial3.write(_RESUME_);
-    // Esto funciona xq se envia la primer fila, caso contrario sera necesario agregar logica adicional para recibir resultado
-    if (Serial3.readBytes(C, n * bs) != n * bs) {
-      Serial.print("Error recibiendo bloque resultado");
-      error();
+    // Si finalizo pero faltan resultados del esclavo lo espera
+    while (esperando_resultados) {
+      while (Serial3.read() != _JOB_REQ_) { ; }
+      enviarTrabajo();
     }
+    //informar tiempo de ejecución
+    elapsed_time = millis() - start_time;
+    Serial.print("Tiempo transcurrido: ");
+    Serial.print(elapsed_time);
+    Serial.println(" ms");
+
     validar();
   }
 }
--- MEGA_ino/src/main.cpp
+++ MEGA_ino/src/main.cpp
@@ -5,15 +5,15 @@
 */
 #include <Arduino.h>
 
-// Bytes reservados para control de flujo de la transmisión
-#define _WAIT_ 255 // Byte reservado para indicar al maestro que debe esperar
-#define _RESUME_ 254 // Byte reservado para indicar al maestro que puede continuar enviando información
+// Byte reservado para control de flujo de la transmisión
+#define _JOB_REQ_ 255
 
 uint8_t* A; // Fila de bloques A (viene de una matriz ordenada por filas)
 uint8_t* B; // Columna de bloques B (viene de una matriz ordenada por columnas)
 uint8_t* C; // Matriz resultado
 uint8_t n = 0; // Cantidad de bytes a recibir
 uint8_t bs = 0; // Tamaño de bloque
+uint8_t col_procesadas = 0; // Lleva la cuenta de las columnas procesadas para saber cuando tiene que pedir una fila de bloques A
 
 // Parpadea el LED integrado indefinidamente
 void error() {
@@ -27,9 +27,9 @@ void error() {
 
 void multiplicar() {
   for (uint8_t i = 0; i < bs; i++) {
-    for (uint8_t j = 0; j < n; j++) {
+    for (uint8_t j = 0; j < bs; j++) {
       for (uint8_t k = 0; k < n; k++) {
-        C[i * n + j] += A[i * n + k] * B[j * n + k];
+        C[i * bs + j] += A[i * n + k] * B[j * n + k];
       }
     }
   }
@@ -37,82 +37,96 @@ void multiplicar() {
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); // LED que parpadea para indicar estado de error
-  Serial.begin(115200); // Consola serie para debug
-  Serial3.begin(115200); // Comunicacion serie con otro micro
+  Serial.begin(115200UL); // Consola serie para debug
+  Serial3.begin(250000UL); // Comunicacion serie con otro micro
+  Serial3.setTimeout(10000); // Timeout de 10 seg
   Serial.println("Esperando valores iniciales..");
-  Serial3.setTimeout(10000); // Timeout de 3 seg
-
   // Obtener valores para alocar memoria
   char bytes[2];
   if (Serial3.readBytes(bytes, 2) != 2) {
     Serial.println("Timeout");
     error(); // Bloqueante con timeout
   }
-  Serial3.write(_WAIT_); // Indicar al maestro que espere alocación de memoria
 
   n = bytes[0];
   bs = bytes[1];
-  Serial.print("Recibi n y bs: ");
-  Serial.print(n);
-  Serial.print(", ");
-  Serial.println(bs);
+  // Serial.print("Recibi n y bs: ");
+  // Serial.print(n);
+  // Serial.print(", ");
+  // Serial.println(bs);
   A = (uint8_t*)malloc(n * bs * sizeof(uint8_t)); // Fila de bloques A
-  B = (uint8_t*)malloc(n * n * sizeof(uint8_t)); // Matriz B
-  C = (uint8_t*)calloc(n * bs, sizeof(uint8_t)); // Fila de bloques C
+  B = (uint8_t*)malloc(n * bs * sizeof(uint8_t)); // Matriz B
+  C = (uint8_t*)calloc(bs * bs, sizeof(uint8_t)); // Fila de bloques C
   if (A == NULL || B == NULL || C == NULL) error();
-
-  Serial3.write(_RESUME_);
-  if (Serial3.readBytes(A, n * bs) != n * bs) {
-    Serial.println("Timeout recibiendo A");
-    error(); // Bloqueante con timeout
-  }
-  if (Serial3.readBytes(B, n * n) != n * n) {
-    Serial.println("Timeout recibiendo B");
-    error(); // Bloqueante con timeout
-  }
-  
-  Serial.println("Recibi las matrices");
-  for (int i = 0; i < bs; i++) {
-    Serial.print("A: ");
-    for (int j = 0; j < n; j++) {
-      Serial.print(A[i * n + j]);
-      Serial.print(" ");
-    }
-    Serial.print("\n");
-  }
-  Serial.println();
-  for (int i = 0; i < n; i++) {
-    Serial.print("B: ");
-    for (int j = 0; j < n; j++) {
-      Serial.print(B[i * n + j]);
-      Serial.print(" ");
-    }
-    Serial.print("\n");
-  }
+  // Pedir trabajo
+  Serial3.write(_JOB_REQ_);
 }
 
 void loop() {
-  multiplicar();
-  Serial.println("Se termino el calculo");
-  // Esperar que el maestro este listo para recibir
-  while (Serial3.read() != _RESUME_) { ; }
-  for (int i = 0; i < n * bs; i++) {
-    Serial3.write(C[i]);
-  }
-  Serial.println("Se envio el resultado");
-  // Imprimir resultado
-  for (int i = 0; i < bs; i++) {
-    Serial.print("C: ");
-    for (int j = 0; j < n; j++) {
-      Serial.print(C[i * n + j]);
-      Serial.print(" ");
+  // Si debe comenzar a procesar una fila nueva
+  if (col_procesadas == 0) {
+    // Recibir filas A
+    if (Serial3.readBytes(A, n * bs) != n * bs) {
+      Serial.println("Timeout recibiendo A");
+      error();
     }
-    Serial.print('\n');
+  }
+  // Recibir columnas B
+  if (Serial3.readBytes(B, n * bs) != n * bs) {
+    Serial.println("Timeout recibiendo B");
+    error();
+  }
+  // Imprimir matrices recibidas
+  // if (col_procesadas == 0) {
+  //   // Imprimir filas A
+  //   Serial.println("Recibi las matrices");
+  //   for (int i = 0; i < bs; i++) {
+  //     Serial.print("A: ");
+  //     for (int j = 0; j < n; j++) {
+  //       Serial.print(A[i * n + j]);
+  //       Serial.print(" ");
+  //     }
+  //     Serial.print("\n");
+  //   }
+  //   Serial.println();
+  // }
+  // // Imprimir columnas B
+  // for (int i = 0; i < bs; i++) {
+  //   Serial.print("B: ");
+  //   for (int j = 0; j < n; j++) {
+  //     Serial.print(B[i * n + j]);
+  //     Serial.print(" ");
+  //   }
+  //   Serial.print("\n");
+  // }
+  multiplicar();
+  col_procesadas++;
+  if(col_procesadas==(n/bs)) col_procesadas=0;
+  // Imprimir matriz resultado
+  // Serial.println("Calculo finalizado");
+  // for (int i = 0; i < bs; i++) {
+  //   for (int j = 0; j < bs; j++) {
+  //     Serial.print(C[i * bs + j]);
+  //     Serial.print(" ");
+  //   }
+  //   Serial.print("\n");
+  // }
+  // Avisar calculo finalizado
+  Serial3.write(_JOB_REQ_);
+  // Esperar a que el maestro este listo para recibir
+  while (Serial3.read() != _JOB_REQ_) { ; }
+  // Enviar bloque resultado
+  for (int i = 0; i < bs; i++) {
+    for (int j = 0; j < bs; j++) {
+      Serial3.write(C[i * bs + j]);
+    }
   }
   // Reiniciar matriz resultado
-  for (int i = 0; i < n * bs; i++) {
-    C[i] = 0;
+  for (int i = 0; i < bs; i++) {
+    for (int j = 0; j < bs; j++) {
+      C[i * bs + j] = 0;
+    }
   }
-  // Esperar mensaje del maestro para volver a enviar el resultado
-  while (Serial3.read() != _RESUME_) { ; }
+  // Esperar a que el maestro este listo para seguir enviando trabajo
+  while (Serial3.read() != _JOB_REQ_) { ; }
 }
